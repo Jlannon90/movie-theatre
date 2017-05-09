ea57ef42edcc03d746ac9659be024c102bca5c33
Calvin Cushman and James Lannon <james@>
2017-05-09 13:19:31 -0700

    add ticketOutput function that adjusts ticket price, add ticketOutput to be displayed

--- js/scripts.js
+++ js/scripts.js
@@ -17,24 +17,36 @@ Ticket.prototype.generateTicket = function() {
 }
 
 
+Ticket.prototype.ticketOutput = function() {
+  var ticketPrice = 12;
 
+  if (this.ticketAge >= 65) {
+     ticketPrice -= 2;
+    }
+  if ((this.ticketTime === "1:15 PM") || (this.ticketTime === "2:30 PM") || (this.ticketTime === "3:00 PM")) {
+    ticketPrice -= 1;
+    }
+  if ((this.movieTicket !== "Raiders of the Lost Gondola") || (this.movieTicket !== "Gasp!")) {
+    ticketPrice += 3;
+    }
 
-
+return ticketPrice;
+};
 
 
 
 // user interface logic
 
 $(document).ready(function() {
-  $("form#new-ticket").submit(function(event) { debugger;
+  $("form#new-ticket").submit(function(event) {
   event.preventDefault();
     var movieChoice = $("input:radio[name=movie]:checked").val();
     var timeChoice = $("input:radio[name=time]:checked").val();
-    var customerAge = $("input#age").val();
+    var customerAge = parseInt($("input#age").val());
 
     var newTicket = new Ticket(movieChoice, timeChoice, customerAge);
 
-    $("ul#tickets").append("<li>" + newTicket.generateTicket() + "</li>");
+    $("ul#tickets").append("<li>" + newTicket.generateTicket() + " $" + newTicket.ticketOutput() + "</li>");
 });
 
 
